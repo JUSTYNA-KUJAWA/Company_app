1f54c242a0711f2807dc678bcbcfc1458360e8e7
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-05-07 15:37:11 +0200

    30.4-fix

--- models/test/departement.crud.test.js
+++ models/test/departement.crud.test.js
@@ -25,13 +25,13 @@ describe('Department', () => {
     it('should return all the data with "find" method', async () => {
       const departments = await Department.find();
       const expectedLength = 2;
-      expect(departments.length).to.be.equal(expectedLength);
+      expect(departments.length).to.be.equal(2);
     });
 
     it('should return a proper document by "name" with "findOne" method', async () => {
       const department = await Department.findOne({ name: 'Department #1' });
       const expectedName = 'Department #1';
-      expect(department.name).to.be.equal(expectedName);
+      expect(department.name).to.be.equal('Department #1');
     });
   
     after(async () => {
